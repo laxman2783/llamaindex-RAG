ae2d49e91016d868dfd8685a098ba28aceede6c8
laxman2783 <laxmant.thota@gmail.com>
2024-02-24 08:29:47 -0500

    Added llamaIndex files

---
+++ llamindex-storage.py
@@ -0,0 +1,40 @@
+import openai
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("OPENAI_KEY")
+PERSIST_DIR = "./storage"
+openai.api_key = SECRET_KEY
+
+
+
+# check if storage already exists
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("sample_data/data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+# either way we can now query the index
+query_engine = index.as_query_engine()
+response = query_engine.query("What is summary of the content?")
+print(response)
+response = query_engine.query("When did Laxman Joined Infosys")
+print(response)
+response3 = query_engine.query("What is Laxman's Linked URL , Email Id and Phone number")
+print(response3)
---
+++ llamindex.py
@@ -0,0 +1,30 @@
+import openai
+import os
+import streamlit as st
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("OPENAI_KEY")
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader,ServiceContext
+from llama_index.core.query_pipeline import QueryPipeline
+from llama_index.core import PromptTemplate
+openai.api_key = SECRET_KEY
+documents = SimpleDirectoryReader("sample_data/data").load_data()
+service_context = ServiceContext.from_defaults(chunk_size=1000)
+
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine(streaming=True)
+input1=st.text_input("Input1", "What is summary of the content?")
+input2=st.text_input("Input2", "When did Laxman Joined Infosys")
+input3=st.text_input("Input3","What is Laxman's Linked URL , Email Id and Phone number")
+
+response = query_engine.query(input1)
+st.text_area("Response",response)
+response1 = query_engine.query(input2)
+st.text_area("Response1",response1)
+response3 = query_engine.query(input3)
+st.text_area("Response3",response3)
